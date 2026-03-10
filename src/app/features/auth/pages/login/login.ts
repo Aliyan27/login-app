207938eab13a90185de518a942e33aa8ecb9faae
Aliyan27 <aliyanmuhammad425@gmail.com>
2026-03-10 23:29:35 +0500

    changes

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// PrimeNG Imports
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule, 
+    ButtonModule, 
+    CheckboxModule, 
+    InputTextModule, 
+    PasswordModule, 
+    CardModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss']
 })
-export class Login {
+export class LoginComponent {
+  isLoading = signal(false);
 
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+      nonNullable: true
+    }),
+    rememberMe: new FormControl(false)
+  });
+
+
+  get f() { return this.loginForm.controls; }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.isLoading.set(true);
+      console.log('Logging in with:', this.loginForm.getRawValue());
+      setTimeout(() => this.isLoading.set(false), 2000);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
